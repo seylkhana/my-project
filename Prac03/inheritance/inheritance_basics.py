6fc2af938eefd30abe3f3f3c3eacb516e0983c5d
seylkhana <abdu02top@gmail.com>
2026-02-10 13:17:45 +0500

    Complete Practice 3: Python inheritance examples

---
+++ Prac03/inheritance/inheritance_basics.py
@@ -0,0 +1,28 @@
+#1
+class Person:
+  def __init__(self, fname, lname):
+    self.firstname = fname
+    self.lastname = lname
+
+  def printname(self):
+    print(self.firstname, self.lastname)
+
+#Use the Person class to create an object, and then execute the printname method:
+
+x = Person("John", "Doe")
+x.printname()
+
+#2
+class Student(Person):
+  pass
+
+x = Student("Mike", "Olsen")
+x.printname()
+
+#3
+class Student(Person):
+  def __init__(self, fname, lname, year):
+    super().__init__(fname, lname)
+    self.graduationyear = year
+
+x = Student("Mike", "Olsen", 2019)
